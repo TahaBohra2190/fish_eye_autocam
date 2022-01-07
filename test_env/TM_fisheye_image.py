3e46d5e7d81760a80d2ce526d3f425fcd9dbe6c0
Taha <thkbohra@gmail.com>
2022-01-07 10:11:12 +0530

    Added some documentation for the square extraction file, also wrote a new test file for testing with different template

--- test_env/TM_fisheye_image.py
+++ test_env/TM_fisheye_image.py
@@ -1,18 +1,26 @@
 """
-This script is meant to check test an image for allignment
-It takes the image captured in the main file and uses template matching to find the position of the symbol in target image 1.
-Th escript will check for allignment by cheking the variance between the position of target symbol between the original image and the camera capture.
-The threshhold is set to 5 pixels lengthwise and width wise. 
+DISCLAIMER : In order to achieve an output user needs to alter values for 3 variables
+            1) In line 14 for the object named sample_image alter the path to master image for your device
+            2) In line 21 for the object named template alter the path to template image for your device
+            3) In line 83 for the imwrite function alter the path to extracted image for your device
+"""
+
+"""
+This script will take the raw image from the fisheye camera as input and gives a cropped out image of only the square in the center
+It takes the image captured in the main file and uses template matching to find the position of the central square in master  image .
 """
 import cv2
 
+#Reading the master image (hardcoded for now)
 sample_image = cv2.imread("images\Master_image_inverted.png",cv2.IMREAD_COLOR)
+
+#funtion to extract the square from master image
 def extract_target(sample_image):
   source = sample_image
+  #importing the template
   template = cv2.imread("images\\rawimage5_template.png")
 
-  #display the image preview
-  #the imshow function takes two inputs, the name of the window and the object to display from
+  #display the image preview, the image_resized variable is used to resize the image into a viewable window of smaller size 
   image_resized = cv2.resize(source, (1280,720))
   cv2.imshow('Preview', image_resized) 
   cv2.waitKey(0) 
@@ -21,28 +29,34 @@ def extract_target(sample_image):
   #the cvtColor function used to convert from RGB to greyscale takes 2 parameters as input, 
   # 1) object to apply color change
   # 2)the color space conversion code 
-  # source = cv2.cvtColor(source, cv2.COLOR_RGB2GRAY)
-  # template = cv2.cvtColor(template, cv2.COLOR_RGB2GRAY) 
+  source = cv2.cvtColor(source, cv2.COLOR_RGB2GRAY)
+  template = cv2.cvtColor(template, cv2.COLOR_RGB2GRAY) 
 
   #Preview image in greyscale
   image_resized = cv2.resize(source, (1280,720))
   cv2.imshow('Preview Greyscaled', image_resized)
   cv2.waitKey(0)  
 
-  #capturing the height and width
+  #capturing the height and width of master image using default .shape method
   height, width , channelss = source.shape
 
+  #capturing the height and width of template using default .shape method
   H, W , C = template.shape
 
-  #a list of all the methods to see results from all the methods
+  #a list of all the methods to see results from all the methods 
   # methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
-  method_sq = [cv2.TM_SQDIFF ]
+  #for now we are only using the squared differential method  
+  method_s = [cv2.TM_SQDIFF ]
 
-  #main loop
-  for method in method_sq:
-    #this loop goes through all the methods of template matching and produces a result for all of them
+  #this loop runs for every variable in the method list containing names of every variable to use
+  for method in method_s:
     src2 = source.copy()
+    #OpenCV funtion for template matching, takes 3 inputs 
+    # 1) Image to search for template in 
+    # 2) Image to use as a template
+    # 3) Method to use for template matching 
     result = cv2.matchTemplate(src2, template, method)
+    #minMaxLoc goes through the matrix and find the minimum and the maximum value. Note that the input image has to be grayscale.
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     print("Found at cordinates" + str(min_loc))
     if method in [cv2.TM_SQDIFF,cv2.TM_CCORR]:
@@ -57,14 +71,15 @@ def extract_target(sample_image):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+  # Using the values in the 
   x_diff = 551-min_loc[0]
   y_diff = 151-min_loc[1]
   print("Offset")
   print("X : "+str(x_diff))
   print("Y : "+str(y_diff))
 
-  #need to patch the invert bug
   region_of_interest = source[ location[1] : location[1] + W , location[0] : location[0] + H  ]
+  # Writing the Area detected in the square detected into an image at the path specified in the first varaible in imwrite function
   cv2.imwrite("images\Cropped_master.png",region_of_interest)
 
 extract_target(sample_image)
@@ -72,25 +87,10 @@ extract_target(sample_image)
 
 
 
-# #capturing the height and width
-# height, width =source.shape
-# height, width 
-
-# H, W = template.shape
-# H, W 
-
-# #a list of all the methods to see results from all the methods
-# # methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
-# method_sq = [cv2.TM_SQDIFF]
 
 
 
-# x_diff = 269-min_loc[0]
-# y_diff = 177-min_loc[1]
-# if min_loc == (269,183):
-#     print("image is alligned")
-# else:
-#     print("Not alligned")
-# print(location)
+
+
 
 
